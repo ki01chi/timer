062a7b21e3be36230f6da18718a3f3006e4afb4a
chisato <kitada.c08@gmail.com>
2020-10-19 14:56:12 +0900

    それぞれ別の動きをするよう修正しました

--- main.js
+++ main.js
@@ -1,108 +1,72 @@
 $(() => {
 
-  const countBox = $('#countBox');
-  const start = $('#start');
-  const stop = $('#stop');
-  const reset = $('#reset');
+  $(".element").each(function () {
+    const countBox = $(this).children('.count');
+    const start = $(this).children('.start-btn');
+    const stop = $(this).children('.stop-btn');
+    const reset = $(this).children('.reset-btn');
+    const display = $(this).children('.display');
+    //タイマーの秒数
+    let setTime = $(this).children(".count").data("time");
+    //一時停止した時の秒数
+    let poseTime = setTime;
+    //残りの秒数
+    let timeLeft = setTime;
+    //setIntervalのための変数
+    let testTimer;
 
-  //タイマーの秒数
-  let setTime = 10;
-  //一時停止した時の秒数
-  let poseTime = setTime;
-  //残りの秒数
-  let timeLeft = setTime;
-  //setIntervalのための変数
-  let testTimer;
+    let minutes;
+    let seconds;
 
-  class KitchenTimer {
-    constructor(setTime, poseTime, timeLeft, testTimer) {
-      this.setTime = 0;
-      this.poseTime = setTime;
-      this.timeLeft = setTime;
-      this.testTimer;
-    }
+    //残りの秒数を表示する関数
+    const displayText = () => {
+      countBox.text(timeLeft);
+      minutes = Math.floor(timeLeft / 60);
+      seconds = timeLeft % 60;
+      if (String(minutes).length == 1)
+        minutes = "0" + String(minutes)
+      if (String(seconds).length == 1)
+        seconds = "0" + String(seconds)
+      display.text(minutes + ":" + seconds);
+    };
+    //1ずつカウントダウンする関数
+    const countDown = () => {
+      timeLeft--;
+      poseTime = timeLeft;
+      displayText();
+    };
+    //カウントをストップする関数
+    const stopCount = () => {
+      clearInterval(testTimer);
+    };
+    //1000ミリ秒ごとに処理を繰り返す関数
+    const timerStart = () => {
+      testTimer = setInterval(function () {
+        if (timeLeft <= 0)
+          clearInterval(testTimer);
+        else
+          countDown();
+      }, 1000);
+      return;
+    };
 
-  
-  }
-
-
-
-
-  //ミリ秒を分に変換________________________________________
-  // function computeDuration(ms){
-  //   let h = String(Math.floor(ms / 3600000) + 100).substring(1);
-  //   console.log(String(Math.floor(ms / 3600000) + 100).substring(1));
-
-  //   let m = String(Math.floor((ms - 0 * 3600000)/60000)+ 100).substring(2);
-
-  //   console.log(String(Math.floor((ms - 0 * 3600000)/60000)+ 100).substring(2));
-
-  //   let s = String(Math.round((ms - 0 * 3600000 - m * 60000)/1000)+ 100).substring(1);
-
-  //   console.log(String(Math.round((ms - 0 * 3600000 - m * 60000)/1000)+ 100).substring(1))
-
-  //   return m + ':' + s;
-  // }
-
-  // let compute = computeDuration(360000);
-  //__________________________________________________________
-
-  //残りの秒数を表示する関数
-  const displayText = () => {
-    countBox.text(timeLeft);
-  };
-
-  //1ずつカウントダウンする関数
-  const countDown = () => {
-    timeLeft--;
-    poseTime = timeLeft;
     displayText();
-  };
-
-  //カウントをストップする関数
-  const stopCount = () => {
-    clearInterval(testTimer);
-  };
-
-  //1000ミリ秒ごとに処理を繰り返す関数
-  const timerStart = () => {
-
-    testTimer = setInterval(function () {
-      if (timeLeft <= 0) {
-        clearInterval(testTimer);
-      }
-      else {
-        countDown();
-      }
-    }, 1000);
-
-    return;
-  };
-
-  //_____________________________________________________
-
-  displayText();
-
-  //ボタンを押したらカウントダウンスタート
-  start.on('click', () => {
-    stopCount();
-    timeLeft = poseTime;
-    console.log(poseTime);
-    console.log(timeLeft);
-    displayText();
-    timerStart();
-    console.log(poseTime);
-    console.log(timeLeft);
-  });
-
-  stop.on('click', () => {
-    stopCount();
-  });
-
-  reset.on('click', () => {
-    stopCount();
-    timeLeft = poseTime = setTime;
-    displayText();
-  });
-
+    //ボタンを押したらカウントダウンスタート
+    start.on('click', () => {
+      stopCount();
+      timeLeft = poseTime;
+      displayText();
+      timerStart();
+    });
+    //ボタンを押したらカウントストップ
+    stop.on('click', () => {
+      stopCount();
+    });
+    //ボタンを押したらカウントリセット
+    reset.on('click', () => {
+      stopCount();
+      timeLeft = poseTime = setTime;
+      displayText();
+    });
+  })
 });
